3f6124cd0b09b379a9a704a2c33a3eb54178a147
Anthony Scopatz <scopatz@gmail.com>
2019-10-11 17:36:49 -0400

    more qterminal

--- recipes/qterminal/build.sh
+++ recipes/qterminal/build.sh
@@ -3,7 +3,9 @@ set -ex
 
 mkdir -p build/
 pushd build
-cmake .. -DCMAKE_INSTALL_PREFIX="${PREFIX}"
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release
 make
 make check
 make install
