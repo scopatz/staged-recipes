b4df5a678b0e761511238eeb54a4e5d84201ce60
Anthony Scopatz <scopatz@gmail.com>
2020-01-25 16:55:07 -0500

    initial multimc5

---
+++ recipes/multimc5/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+mkdir -p build
+pushd build
+cmake \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  ..
+make -j ${CPU_COUNT}
+make install
+popd
